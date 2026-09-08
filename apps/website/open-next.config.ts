4a48960f520193ce7c8d4143703bb4d9060f8729
Roman Bakurov <ronbakurov@gmail.com>
2026-09-08 23:01:01 +0200

    revert(website): remove experimental skew protection

--- apps/website/open-next.config.ts
+++ apps/website/open-next.config.ts
@@ -1,15 +1,7 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 import staticAssetsIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache';
 
-const config = defineCloudflareConfig({
+export default defineCloudflareConfig({
   incrementalCache: staticAssetsIncrementalCache,
   enableCacheInterception: true,
 });
-
-config.cloudflare!.skewProtection = {
-  enabled: true,
-  maxNumberOfVersions: 10,
-  maxVersionAgeDays: 7,
-};
-
-export default config;
