c468b0bd9c3e7ef75faa46db8ce328ef1e1e00df
Roman Bakurov <ronbakurov@gmail.com>
2026-09-17 18:40:19 +0200

    fix(react-native): use native view instance refs

--- packages/react-native/src/components/RadioGroup/Item/RadioGroupItem.tsx
+++ packages/react-native/src/components/RadioGroup/Item/RadioGroupItem.tsx
@@ -1,12 +1,12 @@
 import { forwardRef } from 'react';
 
-import type { View } from 'react-native';
+import type { ViewInstance } from 'react-native';
 
 import { Radio } from '../../../primitives/Radio';
 
 import type { RadioGroupItemProps } from './types';
 
-export const RadioGroupItem = forwardRef<View, RadioGroupItemProps>(
+export const RadioGroupItem = forwardRef<ViewInstance, RadioGroupItemProps>(
   (props, ref) => <Radio {...props} ref={ref} />
 );
 
